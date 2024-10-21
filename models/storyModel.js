4ab8ff23ec67b39a8943af987c99857fe5ebdce3
Lê Hồng Ngân 20036651 <lehongngan.998@gmail.com>
2024-10-21 21:59:57 +0700

    change stories model

--- models/storyModel.js
+++ models/storyModel.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
+const wordsSchema = new mongoose.Schema({
+    word: { type: String, required: true },
+    meaning: { type: String, required: true },
+});
 
 const  storySchema = new mongoose.Schema({
     nameVn: { type: String, required: true },
     nameEn: { type: String, required: true },
     image: { type: String, required: true },
     content: { type: String, required: true },
+    words:[wordsSchema],
     createdAt: { type: Date, default: Date.now },
 })
 
